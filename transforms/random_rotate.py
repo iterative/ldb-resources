28fa23eba56e655a737d74a8e48c607c241e7c5f
Jon Burdo <jon@jonburdo.com>
2022-06-29 11:42:15 -0400

    feat(transform): crop rotated images to original aspect ratio

--- transforms/random_rotate.py
+++ transforms/random_rotate.py
@@ -4,6 +4,7 @@ import os
 import random
 import shutil
 import sys
+from math import cos, pi, sin
 from typing import Dict, Sequence
 
 from PIL import Image
@@ -35,7 +36,7 @@ def main(inp: Dict[str, str], argv: Sequence[str] = ()) -> None:
         ext = ""
 
     str_args = "-".join(map(str, transform_args))
-    new_image = orig_image.rotate(degrees)
+    new_image = rotate_and_crop(orig_image, degrees)
     file_name_base = f"{file_name_base}-{transform_name}-{str_args}".replace(
         ".",
         "-",
@@ -44,6 +45,31 @@ def main(inp: Dict[str, str], argv: Sequence[str] = ()) -> None:
     annot_file_path = os.path.join(output_dir, f"{file_name_base}.json")
     new_image.save(obj_file_path, format=fmt)
     shutil.copy2(annotation_path, annot_file_path)
+
+
+def rotate_and_crop(image, degrees):
+    """
+    Rotate the given image by `degrees`, and crop the resulting image
+    to the largest rectangle with the original aspect ratio.
+    """
+    rotated_image = image.rotate(degrees, expand=True)
+    w1, h1 = image.size
+    w2, h2 = rotated_image.size
+    r1 = w1 / h1
+    r2 = w2 / h2
+    angle = abs(degrees) * pi / 180
+    if w1 < h1:
+        total_height = w1 / r2
+    else:
+        total_height = h1
+    h = abs(total_height / (r1 * abs(sin(angle)) + abs(cos(angle))))
+    w = h * r1
+
+    x1 = (w2 - w) / 2
+    x2 = w2 - x1
+    y1 = (h2 - h) / 2
+    y2 = h2 - y1
+    return rotated_image.crop([x1, y1, x2, y2])
 
 
 if __name__ == "__main__":
--- transforms/rotate.py
+++ transforms/rotate.py
@@ -3,6 +3,7 @@ import json
 import os
 import shutil
 import sys
+from math import cos, pi, sin
 from typing import Dict, Sequence
 
 from PIL import Image
@@ -24,7 +25,7 @@ def main(inp: Dict[str, str], argv: Sequence[str] = ()) -> None:
         fmt = orig_image.format or "PNG"
         ext = ""
     for n in transform_args:
-        new_image = orig_image.rotate(int(n), expand=True)
+        new_image = rotate_and_crop(orig_image, int(n))
         file_name_base = f"{file_name_base}-{transform_name}-{n}".replace(
             ".",
             "-",
@@ -33,6 +34,31 @@ def main(inp: Dict[str, str], argv: Sequence[str] = ()) -> None:
         annot_file_path = os.path.join(output_dir, f"{file_name_base}.json")
         new_image.save(obj_file_path, format=fmt)
         shutil.copy2(annotation_path, annot_file_path)
+
+
+def rotate_and_crop(image, degrees):
+    """
+    Rotate the given image by `degrees`, and crop the resulting image
+    to the largest rectangle with the original aspect ratio.
+    """
+    rotated_image = image.rotate(degrees, expand=True)
+    w1, h1 = image.size
+    w2, h2 = rotated_image.size
+    r1 = w1 / h1
+    r2 = w2 / h2
+    angle = abs(degrees) * pi / 180
+    if w1 < h1:
+        total_height = w1 / r2
+    else:
+        total_height = h1
+    h = abs(total_height / (r1 * abs(sin(angle)) + abs(cos(angle))))
+    w = h * r1
+
+    x1 = (w2 - w) / 2
+    x2 = w2 - x1
+    y1 = (h2 - h) / 2
+    y2 = h2 - y1
+    return rotated_image.crop([x1, y1, x2, y2])
 
 
 if __name__ == "__main__":
